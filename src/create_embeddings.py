bb3964f558c236d10fc0f2057ac3f7e409d97865
alonge9500 <alongedaniel19@gmail.com>
2025-02-08 18:33:55 +0100

    Successfully Create Embeddings

---
+++ src/create_embeddings.py
@@ -0,0 +1,36 @@
+import json
+import os
+from sentence_transformers import SentenceTransformer
+
+
+def generate_embeddings(json_file, output_file, model_name="all-MiniLM-L6-v2"):
+
+    """
+    INPUT:  json_file The Text data Without Embedding
+            output_file The Text file to be created including embeddings
+            model_name The sentence transformer file to be use for embedding
+
+            Return NONE
+
+            Function: Concatenate both section and question and return a json file 
+            Including their embeddings
+    
+    """
+    model = SentenceTransformer(model_name)
+    input_path = os.path.join("documents", json_file)
+    output_path = os.path.join("documents", output_file)
+    
+    with open(input_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    
+    for section in data:
+        text_for_embedding = f"{section['Section']}: {section['Content']}"
+        section["Embedding"] = model.encode(text_for_embedding).tolist()
+
+    print(len(section["Embedding"]))
+    
+    with open(output_path, 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
+
+# Example usage
+generate_embeddings("output.json", "content_section_embeddings.json")
---
+++ src/create_json.py
@@ -0,0 +1,43 @@
+import json
+import os
+
+def convert_txt_to_json(input_file, output_file):
+    """
+    input: A text file, an output file
+    return: Json format of input text
+    
+
+    function: Receive a .txt file and convert it to appropriate json format
+    
+    """
+    input_path = os.path.join("documents", input_file)
+    output_path = os.path.join("documents", output_file)
+    sections = []
+    with open(input_path, 'r', encoding='utf-8') as file:
+        section = {}
+        for line in file:
+            line = line.strip()
+            if not line:
+                continue
+            
+            if line.startswith("Section :"):
+                if section:  
+                    sections.append(section)
+                section = {"Section": line.split(":", 1)[1].strip()}
+            elif line.startswith("Id :"):
+                section["Id"] = int(line.split(":", 1)[1].strip())
+            elif line.startswith("Content :"):
+                section["Content"] = line.split(":", 1)[1].strip()
+            else:
+                section["Content"] += " " + line  
+        if section: 
+            sections.append(section)
+    
+    with open(output_path, 'w', encoding='utf-8') as json_file:
+        json.dump(sections, json_file, indent=4, ensure_ascii=False)
+
+    return section
+
+# Example usage
+result = convert_txt_to_json("ndpa.txt", "ndpa.json")
+#print(result)
